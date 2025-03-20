228df3eba6ba620691a029048a5af54f28f4c5ad
harshsinghh26 <harshsi343@gmail.com>
2025-03-20 21:07:18 +0530

    adedd utils middlewares and some packages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from 'dotenv';
+// const dotenv = require('.dotenv').config({ path: './env' });
 import dbConnection from './db/index.js';
+import app from './app.js';
 
 dotenv.config({
   path: './env',
 });
 
-dbConnection();
+dbConnection()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MONGODB connection failed!!');
+  });
